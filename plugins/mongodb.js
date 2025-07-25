a95f9a97614eb585104e252680c7f2e4fd733205
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-25 10:53:53 +0530

    added custom plugin for mongoDB

---
+++ plugins/mongodb.js
@@ -0,0 +1,14 @@
+const fp = require("fastify-plugin");
+const mongoose = require("mongoose");
+
+
+module.exports = fp(async (fastify, opts) => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    fastify.decorate("mongoose", mongoose);
+    fastify.log.ingo("MongoDB connected!");
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+});
