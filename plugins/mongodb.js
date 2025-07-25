4a1dc9c511cd8d25bedeece16239d7e3f0c79852
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-25 11:05:19 +0530

    fixed mongo connection bug

--- plugins/mongodb.js
+++ plugins/mongodb.js
@@ -6,7 +6,7 @@ module.exports = fp(async (fastify, opts) => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     fastify.decorate("mongoose", mongoose);
-    fastify.log.ingo("MongoDB connected!");
+    fastify.log.info("MongoDB connected!");
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
